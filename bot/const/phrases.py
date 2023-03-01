f23276d830ee464a36b8ad33a70de94af6c40329
Andrew <vesell4123@mail.ru>
2023-03-01 16:37:28 +0300

    add daily draw phrase

--- bot/const/phrases.py
+++ bot/const/phrases.py
@@ -1,6 +1,15 @@
-async def phrase_for_start_first_greeting(data):
+async def phrase_for_start_first_greeting(data: str) -> str:
     text = \
         f"""<b>Привет {data}</b> \n\nДанный бот создан для помощи нашим братьям Армянам. """ + \
         """Каждый день здесь будут раздаваться деньги, собранные со всех желающих.""" + \
         """Каждый, кто внес N рублей, автоматически становится участником розыгрыша."""
+    return text
+
+
+async def phrase_of_daily_draw(data: dict) -> str:
+    if "winner_tag" in data:
+        text = f"Тег победителя: <b>@{data['winner_tag']}\n</b>" \
+               f"Сумма выигрыша: <b>{data['total_bank']}</b>"
+    else:
+        text = "Участники отсутствуют!"
     return text
