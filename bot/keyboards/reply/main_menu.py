f21a2076bc3de1797264489699f953ef8a751b75
Andrew <vesell4123@mail.ru>
2023-03-05 23:20:09 +0300

    temporarily removing the button

--- bot/keyboards/reply/main_menu.py
+++ bot/keyboards/reply/main_menu.py
@@ -6,6 +6,6 @@ async def user_main_markup() -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     builder.row(
         KeyboardButton(text="Розыгрыш"),
-        KeyboardButton(text="Попросить помощь")
+        # KeyboardButton(text="Попросить помощь")
     )
     return builder.as_markup(resize_keyboard=True)
