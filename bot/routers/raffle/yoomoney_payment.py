103a56ce4f416cc57bea756534dc2ca8c5d3f3be
Andrew <vesell4123@mail.ru>
2023-03-05 23:22:25 +0300

    verification of payment of payments and sending of payment

--- bot/routers/raffle/yoomoney_payment.py
+++ bot/routers/raffle/yoomoney_payment.py
@@ -1,6 +1,11 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import insert, select
+
+from app.database.models import Raffle, User
+from bot.keyboards.inline.raffle import back_to_raffle_menu
 from bot.keyboards.inline.payments import (
     payment_methods,
     amount_money,
@@ -8,7 +13,9 @@ from bot.keyboards.inline.payments import (
 )
 from bot.const.phrases import (
     phrase_enter_payment_amount,
-    phrase_choose_payment_method
+    phrase_choose_payment_method,
+    phrase_participation_in_drawing,
+    phrase_making_payment
 )
 
 from libs.yoomoney.client import Client
@@ -36,30 +43,28 @@ async def choose_payment(call: CallbackQuery):
 @yoo_payment.callback_query(lambda call: call.data.split(":")[0] == "UMoney")
 async def send_payment_methods(call: CallbackQuery) -> None:
     await call.message.edit_text(
-        "Совершите оплату и проверьте её!",
+        text=await phrase_making_payment(),
         reply_markup=await yoomoney_check_payment(call.data)
     )
 
 
 @yoo_payment.callback_query(lambda call: call.data.split(":")[0] == "yoomoney_check")
-async def check_payment(call: CallbackQuery) -> None:
+async def check_payment(call: CallbackQuery, session: AsyncSession) -> None:
     client = Client(YOOMONEY_TOKEN)
     label_payment_uuid = call.data.split(":")[1]
     history = client.operation_history(label=label_payment_uuid)
     for operation in history.operations:
         if operation.label == label_payment_uuid and operation.status == "success":
-            print("payment success!!!!!!!!!!!!!")
-
-            print("Operation:", operation.operation_id)
-            print("\tStatus     -->", operation.status)
-            print("\tDatetime   -->", operation.datetime)
-            print("\tTitle      -->", operation.title)
-            print("\tPattern id -->", operation.pattern_id)
-            print("\tDirection  -->", operation.direction)
-            print("\tAmount     -->", operation.amount)
-            print("\tLabel      -->", operation.label)
-            print("\tType       -->", operation.type)
-
-        else:
-            print("payment not success!!!!!!!!!!!!!")
-
+            user_id = (await session.execute(
+                select(User.id)
+                .where(User.tg_id.__eq__(call.message.chat.id))
+            )).scalar()
+            await session.execute(
+                insert(Raffle)
+                .values(user_id=user_id, donated=operation.amount)
+            )
+            await session.commit()
+            await call.message.edit_text(
+                text=await phrase_participation_in_drawing(),
+                reply_markup=await back_to_raffle_menu()
+            )
